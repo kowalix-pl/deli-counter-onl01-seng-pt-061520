1681781104ac8a8f9c4aa93454e6f03d87826dbc
kowalix007 L.K <kowalix007@gmail.com>
2020-06-18 15:02:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+require 'pry'
+def line(katz_deli)
+
+unless katz_deli == []
+ katz_deli.each_with_index {|person, index|
+puts "The line is currently: #{index.to_i + 1}. #{person}."}
+else 
+puts "The line is currently empty."
+end
+end
+
+def take_a_number(katz_deli,name)
+case katz_deli.size
+  when katz_deli.size == 0
+  puts "Welcome, #{name}. You are number 1 in line."
+else 
+  size = katz_deli.size
+  puts "Welcome, #{name}. You are number #{size + 1} in line."
+end 
+end
+
